26c60217e1a44c69fd06871f1ea43e6bda0a2874
Stefano Casonato <121874632+khr0me@users.noreply.github.com>
2025-02-12 13:09:09 +0100

    testi home page

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,211 @@ import Navbar from "./components/Navbar.vue";
 
   <Header>Home - Blog Viaggi di Gulliver </Header>
 
-  <main>
-    <Dialog> Pazzerella Eraclea Mare </Dialog>
+  <hr color="green-800" />
 
-    <hr color="green-800" />
+  <main>
+    <Dialog>
+      <i>
+        &lt;&lt; Fin dai primi decenni del Settecento, si diffuse in tutta
+        Europa l’esigenza di superare una cultura ormai recepita come retorica e
+        anacronistica, per affermare invece un pensiero moderno e fondato sulla
+        ragione… Questa aspirazione è destinata ad investire e a trasformare
+        radicalmente le idee, i costumi, le istituzioni politiche e le forme
+        nella vità della società &gt;&gt;
+      </i>
+    </Dialog>
+
+    <Dialog>
+      Questo è ciò che scrisse Pietro Verri, il 26 maggio 1764, nel nuovo
+      periodico “il Caffè” , in collaborazione con suo fratello, Alessandro
+      Verri, e il famoso filosofo e letterato dei nostri tempi, Cesare Beccaria.
+      Come dice Verri, nell’articolo di apertura di questo periodico, il suo
+      giornale conterrà “cose varie, cose disparatissime, cose inedite, cose
+      fatte da diversi autori, cose tutte dirette alla pubblica utilità”
+      Facendoci già capire le sue tendenze illuministiche. L’illuminismo… Uno
+      dei movimenti più caratteristici dei nostri tempi che, se non sapevate, fu
+      il primo movimento culturale che scelse da sé il proprio nome e la propria
+      definizione! In virtù della consapevolezza di appartenere ad un “secolo
+      dei lumi”, cioè ad un periodo in cui il sapere può illuminare le tenebre
+      dell’ignoranza e riformare la società in ogni suo aspetto. Proprio per
+      questo, oggi, 26 ottobre 1786, in occasione dei 60 anni dalla
+      pubblicazione de “I viaggi di Gulliver” di Jonathan Swift, vogliamo
+      condividere con voi le nostre interviste inedite ai rappresentanti del
+      Caffè. Scopriremo insieme le loro riflessioni sull'impatto culturale di
+      quest'opera, esplorando come abbia influenzato il pensiero critico e la
+      letteratura del settecento, rivelando curiosità e aneddoti che vi
+      sorprenderanno. Un viaggio attraverso il tempo, la satira e la visione di
+      un mondo che, in fondo, non è mai stato così lontano dal nostro. Facciamo,
+      quindi, entrare il nostro primo ospite… Immanuel Kant
+    </Dialog>
+    <Dialog>
+      Kant… ci parli un p’ò di lei “Nasco in Prussia, più precisamente a
+      Konigsberg nel 1724, da una famiglia di modeste origini. Fu proprio grazie
+      ai miei genitori che ricevetti un'educazione “etico-religiosa”. Dopo
+      essermi laureato inizia a guadagnare primi spicci come precettore
+      domestico, anche se poi, a quasi 30 anni inizia a cimentarmi in campi
+      didattici più vasti. Ottenni poi la cattedra, nel 1770 di Logica e
+      Metafisica presso l’Università di Konigsberg… Fù proprio in questo periodo
+      che inizia a scrivere tra le mie più importanti opere.
+    </Dialog>
+    <Dialog>
+      Nel “coraggio del sapere”, da lei pubblicato ormai nel 1784, dai una tua
+      interpretazione ai precetti dell’Illuminismo. Perchè questo titolo?&nbsp;
+      “Citando me stesso <i>&lt;&lt;L’illuminismo è l’uscita da uno stato di
+      minorità che egli deve imputare a se stesso&gt;&gt;.</i> è solo grazie
+      all’illuminismo che l’umanità si libera dai condizionamenti e dalla
+      mancanza di coraggio che per secoli hanno soffocato le sue potenzialità.
+      Il titolo di questo mio articolo deriva infatti dalla massima latina
+      <i>&lt;&lt;Sapere Aude&gt;&gt;</i>, cioè l'audacia di pensare con la propria
+      testa, rifiutando i vincoli imposti da ogni principio di autorità. Come
+      dico sempre ai giovani: Abbi il coraggio di servirti della tua propria
+      intelligenza! “
+    </Dialog>
+    <Dialog>
+      Sappiamo sei un estimatore de “I Viaggi di Gulliver”, c’è qualcosa che ti
+      ha colpito in particolare? “Sì, ho sempre avuto una passione per il genere
+      fantastico e sono un grande ammiratore delle opere di Swift. È davvero
+      difficile per me scegliere un tema tra i tanti che emergono nella sua
+      opera, ma se dovessi centrarne uno in particolare, parlerei del coraggio e
+      forza di Gulliver nel mettere in discussione “l’ordine stabilito”. Per
+      superare la condizione di subalternità intellettuale, l’uomo deve
+      liberarsi dalle catene della tradizione, non esitando a criticare le
+      opzioni di norma accettate come valide, proprio come fece Gulliver. Egli
+      si trovò davanti mondi che mettevano in discussione la sua percezione di
+      realtà, come i Lilliput, dove il loro conflitto politico era ridicolo e
+      sproporzionato, oltre ad essere cruento; oppure nell’isola di Brobdingnag,
+      ove la prospettiva era “specchiata”, in una società formata da giganti, ma
+      che rispettava alla perfezione i concetti del movimento illuminista.
+      Proprio in quest’ultima isola, la società inglese di Gulliver (che sarebbe
+      la nostra) viene derisa, perché caratterizzata da arroganza, autoillusione
+      e presunzione di superiorità delle civiltà umane rispetto alle altre.
+      Concordo come, ognuno di noi, debba vedere oltre le limitazioni culturali
+      e storiche, per arrivare a una comprensione più profonda e universale del
+      mondo”
+    </Dialog>
+    <Dialog>
+      Grazie per il suo tempo signor Kant. C’è qualcosa che vuole dire per
+      chiudere questa intervista? “Grazie a voi, prima di tutto. Sono felice di
+      avervi aiutato nel vostro… Come si chiama? Blog? Scusi, è che non sono
+      molto informato su queste nuove tecnologie, penso che i pamphlet rimangano
+      ancora la principale fonte di informazione per il terzo stato, ma sarò di
+      certo speranzoso che questo “Blog” permetterà di raggiungere più persone
+      di quelle raggiunte con la nostra Repubblica delle lettere. Ringrazio
+      anche voi, lettori, per essere stati così interessati a riguardo della mia
+      opinione, e vi invito a continuare la vostra vità seguendo 3 punti di
+      riferimento, o come preferisco chiamarle io chiamarle, le tre
+      “meraviglie”, quali: Il cielo stellato sopra di me, la legge morale in me
+      e il mondo del diritto fuori di me. Arrivederla buon uomo!”
+    </Dialog>
+    <Dialog>
+      E così finisce la nostra prima intervista con Immanuel Kant. Un
+      personaggio a dir poco interessante! Se volete proprio sapere, lui stesso
+      ci ha parlato (privatamente) di un’opera che da qui a poco avrebbe
+      pubblicato: “La critica della ragion pratica” , ma… è ancora un “Work in
+      progress”, usando un inglesismo tipico di Giorgio III . Bando alle ciance,
+      continuiamo ad esplorare il mondo dell’Illuminismo, passando al nostro
+      secondo ospite: Voltaire
+    </Dialog>
+    <Dialog>
+      Voltaire… Non era morto? E perché questo pseudonimo? “Questa è
+      un'intervista oppure un interrogatorio? Che sgarbato… Si, sono morto nel
+      1778, più precisamente il 30 maggio, ma il mio compito come Illuminista
+      non è di certo terminato, per questo sono qui. Per quanto riguarda il mio
+      pseudonimo… è l’anagramma, in latino, del mio cognome, con cui ero
+      conosciuto in gioventù. è più “letterario” e in linea con i principi da me
+      avanzati. Alcuni pensarono che il significato derivasse dalle connotazioni
+      di “velocità” ed "audacia", che, anche se mi fanno onore, non sono
+      purtroppo la vera origine di questo mio omonimo. Comunque, non sono di
+      certo tornato dall’aldilà per essere trattato in questo modo, quindi porgo
+      le mie scuse, ma c’è altro a cui devo dedicarmi in questo momento….”
+    </Dialog>
+    <Dialog>
+      Voltaire! Ci scusi per l’ignoranza…. ci parli un po di lei “...Accetto le
+      sue scuse e, per la cronaca, il mio vero nome è Francois-Marie Arouet.
+      Nasco a Parigi nel 1694, da una famiglia borghese. Ricevetti un'educazione
+      umanistica presso il prestigioso collegio parigino Lois-Le-Grand, tenuto
+      dai Gesuiti; ma mi feci conoscere dopo la pubblicazione della mio primo
+      grande successo: la tragedia Edipo, del 1718, permettendomi di accedere
+      all’alta società francese. Purtroppo, nel 1727, per essermi burlato di un
+      nobile, venni bastonato pubblicamente e rinchiuso nella Bastiglia. Fu poi
+      costretto, settimane dopo ad aver abbandonato la prigione, a lasciare la
+      Francia, recandomi così in Inghilterra, dove stetti per circa tre anni.
+      Qui conobbi una società totalmente estranea a me, venendo in contatto con
+      una realtà politica, sociale, civile e culturalmente diversa da quella
+      francese. Anche se ritornai a Parigi, non rimasi per molto, e ben presto
+      fui obbligato nuovamente ad abbandonare la mia città natale per andarmene,
+      nel 1749 a Berlino, presso Federico II; il quale si considerava mio
+      seguace. Questo idillio tra “discepolo” e “maestro”, però, durò poco ed
+      entrai così nuovamente in patria, su invito di d’Albert e Diderot,
+      collaborando anche alla stesura dell Encyclopédie. Nel 1775 mi stabilì poi
+      in Svizzera, ma per poi tornare definitivamente nel 1778 a Parigi, anno
+      della mia morte“
+    </Dialog>
+    <Dialog>
+      Parlando di una delle sue opere più importanti, il “Tratto della
+      tolleranza” del 1763, che messaggio vuole trasmettere al popolo francese?
+      “L’opera tratta temi a me cari: la tolleranza e la lotta contro il
+      fanatismo religioso. Essa nasce infatti dalla ingiusta condanna a morte,
+      comminata nel 1762, da giudici cattolici, ai danni di un commerciante
+      protestante, Jean Calas, Mi misi così in prima persona per riabilitare la
+      figura di Calas. In particolare nel mio tratto “Gli uomini sono tutti
+      fratelli”, coniugando forza polemica e impegno civile, amore per l’essere
+      umano e constatazione delle mie miserie, faccio un appello verso Dio, un
+      dio eguale a tutti gli esseri, affinché instilli negli individui
+      l’esigenza di convivere nella pace e nel rispetto reciproco. Citando me
+      stesso <i>&lt;&lt;Solo la tolleranza può costruire il fondamento della
+      società civile &gt;&gt;</i>”.
+    </Dialog>
+    <Dialog>
+      Sappiamo che è un estimatore de “I Viaggi di Gulliver”. questa sua vita
+      errante ha fatto sì che si rispecchiasse nell'opera? “Di sicuro ciò ha
+      favorito. Ho sempre amato il genere fantastico e sono sempre stato un
+      grande estimatore delle opere di Swift. Ciò che mi ha colpito maggiormente
+      è stata l’isola degli Houyhnhnm, “Cavalli” i quali loro concetti
+      ideologici rispettavano ciò che avevo sviluppato nel "Trattato sulla
+      Tolleranza” . Non avrei immaginato che un libro scritto con tanto spirito
+      critico potesse, in un certo modo, rispecchiare il pensiero che avevo
+      formulato con altrettanta convinzione. Un isola dove cavalli “illuministi”
+      vivono in una società pari tra di loro, senza alcuna distinzione sociale.
+      Seppur in un contesto straordinario, Swift dipinge un mondo in cui la
+      razza e l’apparenza fisica non separano gli individui, e la società si
+      regge sull’idea di un rispetto universale per la dignità di ogni essere.
+      Come dissi, parlando nel mio tratto “<i>&lt;&lt; Fa si che le piccole
+      differenze tra i vestiti che coprono i nostri deboli corpi, tra tutte le
+      nostre lingue inadeguate, tra tutte le nostre usanze ridicole, tra tutte
+      le nostri leggi imperfette, tra tutte le nostre opinioni insensate, tra
+      tutte le nostre convinzioni così diseguali ai nostri occhi e così uguali
+      davanti a te, insomma tutte queste piccole sfumature che distinguono gli
+      atomi chiamati “uomini” non siano altrettanto segnacoli di odio e di
+      persecuzione&gt;&gt;</i>”; cioè, dal riconoscimento della fragilità ed
+      imperfezione dell’uomo devono nascere il desiderio e la pratica della
+      convivenza e della tolleranza.”
+    </Dialog>
+    <Dialog>
+      Grazie Signor Voltaire, C’è qualcosa che vuole dire per chiudere questa
+      intervista? “Grazie a lei per l’interesse. Spero che questo mio intervento
+      possa avervi aiutato nella vostra opera volta all’unione ed informazione
+      del popolo. Cosa scusi? Tutto ciò verrà pubblicato in un Blog? Guardi, mi
+      scusi per l’ignoranza ma non so proprio cosa sia. Ringrazio calorosamente
+      anche voi, lettori, così interessati a riguardo della mia opinione, e vi
+      invito a seguire la vostra vita apprendendo il più possibile attraverso i
+      vostri sensi. Non esistono idee innate, ma solo frutto di quello che
+      sentiamo. Bisogna accettare l’imperfetta condizione umana con i suoi
+      errori e le sue fragilità. Arrivederla!”
+    </Dialog>
+    <Dialog>
+      E così finisce anche la nostra seconda intervista… Che storia commovente
+      quella di Voltaire! Un viaggiatore, un “disperso”, obbligato quasi per
+      tutta la sua vità ad abbandonare la propria città natale, le proprie
+      tradizioni, solo per seguire una propria ideologia… sto quasi per
+      piangere… Ma non rattristiamoci! Chiamiamo adesso il nostro ultimo ospite…
+      Cesare Beccaria! Cosa? Scusate? Cesare Beccaria non è qui presente? Ma
+      come…? Non gli è arrivato l’invito? Peccato… Cari lettori, mi scuso
+      vivamente per l'inconveniente, ma mi sa che la nostra avventura nel
+      fantastico mondo di Swift debba finire qua… Spero via sia piaciuto, e non
+      dimenticatevi di guardare il nostro Forum! e casomai non vi rivedessi,
+      buon pomeriggio buona sera e buona notte.
+    </Dialog>
   </main>
 
   <footer>
